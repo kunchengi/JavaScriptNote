1149cdff419c5dbde801db74e4dbc520704e72a7
city118 <cityckc@qq.com>
2024-11-24 20:47:07 +0800

    优惠promise

--- principle/promise实现原理.js
+++ principle/promise实现原理.js
@@ -18,6 +18,50 @@
  *      - 如果回调函数抛出异常，则执行新promise的reject方法
  *      - 如果返回值为promise，则执行该promise的then方法，否则执行新promise的resolve方法
  *      - 处理重复引用：如果返回值和当前promise是同一个对象，则抛出异常
+ * 10. 实现catch方法
+ *      - catch方法实际上是then方法的语法糖
+ *      - catch方法等价于then(null, onRejected)
+ * 11. 实现finally方法
+ *      - finally方法无论promise是成功还是失败，都会执行回调函数
+ *      - finally方法等价于then(onFinally,onFinally)
+ * 12. 实现静态方法resolve
+ *      - 如果是promise则直接返回，否则创建成功的promise并返回
+ * 13. 实现静态方法reject
+ *      - 创建失败的promise并返回
+ * 14. 实现静态方法race
+ *      - 遍历所有promise，只要有一个promise解决或拒绝，新promise就解决或拒绝
+ * 15. 实现静态方法all
+ *      - 返回promise实例
+ *      - 判断是否为数组，不是数组返回拒绝的promise，错误信息：Arguments must be iterable
+ *      - 空数组直接返回成功的promise
+ *      - 处理全部解决
+ *          - 记录结果：通过索引设置到新数组中，保证结果的顺序和promise数组的顺序一致
+ *          - 判断全部解决：记录解决的次数，如果次数和promise数组长度一致，则返回成功的promise
+ *      - 处理第一个拒绝
+ * 16. 实现静态方法allSettled
+ *      - 返回promise实例
+ *      - 判断是否为数组，不是数组返回拒绝的promise，错误信息：Arguments must be iterable
+ *      - 空数组直接返回成功的promise
+ *      - 等待所有结果
+ *          - 记录结果：通过索引设置到新数组中，保证结果的顺序和promise数组的顺序一致
+ *          - 处理成功{status:'fulfilled',value:x}
+ *          - 处理失败{status:'rejected',reason:x}
+ *          - 判断结束：记录成功+失败的次数，如果次数和promise数组长度一致，则返回成功的promise
+ * 17. 实现静态方法any
+ *      - 返回promise实例
+ *      - 判断是否为数组，不是数组返回拒绝的promise，错误信息：Arguments must be iterable
+ *      - 传入空数组，拒绝，AggregateError: All promises were rejected--[]
+ *      - 如果有成功，获取最快成功的值
+ *      - 如果都失败，拒绝，AggregateError: All promises were rejected--[失败原因1，失败原因2...]
+ *      - 不传入参数，拒绝，TypeError: undefined is not iterable (cannot read property Symbol(Symbol.iterator))
+ * 18. 测试自己写的promise是否符合规范
+ *      - 暴露deferred方法，返回对象{promise,resolve,reject}
+ *      - 初始化项目，npm init -y
+ *      - 下载测试包：cnpm i promises-aplus-tests -D
+ *      - 配置：package.json代码文件的scripts
+ *          - "testPromise": "promises-aplus-tests principle/promise实现原理.js"
+ *      - 执行：npm run testPromise
+ *      - 错误文档：https://promisesaplus.com/
  */
 
 class Promise1{
@@ -140,10 +184,10 @@ class Promise1{
         // 如果回调不是函数，则赋值一个函数，避免报错
         if (!(onFulfilled instanceof Function)) {
             // 回调函数返回值为当前值，实现值穿透
-            onFulfilled = () => this.PromiseResult;
+            onFulfilled = (res) => res;
         }
         if (!(onRejected instanceof Function)) {
-            onRejected = () => this.PromiseResult;
+            onRejected = (err) => {throw err};
         }
         // 返回一个新的promise,实现链式调用
         // 新promise的状态除了执行异常，其它情况都是解决状态
@@ -266,15 +310,30 @@ class Promise1{
         // 当所有promise都解决时，新promise才解决
         // 无论是否拒绝，都返回一个对象
         return new Promise1((resolve, reject) => {
+            // 如果不是数组，则返回一个拒绝的promise
+            if(!Array.isArray(promises))
+            {
+                reject(new TypeError("Argument is not iterable"));
+            }
+            // 如果数组长度为0，则返回一个解决的promise
+            if(promises.length === 0)
+            {
+                resolve(promises);
+            }
             let results = [];
-            const compoletFunc = (value) => {
-                results.push(value);
-                if(results.length === promises.length){
+            let count = 0;
+            const compoletFunc = (index,isSuccess,value) => {
+                let result = isSuccess ? {status: Promise1.FULFILLED, value} : {status: Promise1.REJECTED, reason: value};
+                results[index] = result;
+                if(++count === promises.length)
+                {
                     resolve(results);
                 }
             };
-            promises.forEach((promise) => {
-                promise.then(compoletFunc,compoletFunc);
+            promises.forEach((promise,index) => {
+                // resolve会把非promise转成promise，promise直接用
+                let realPromise = Promise1.resolve(promise);
+                realPromise.then(compoletFunc.bind(this,index,true),compoletFunc.bind(this,index,false));
             })
         })
     }
@@ -290,11 +349,30 @@ class Promise1{
         // 当所有promise都解决时，新promise才解决
         // 只要有一个promise拒绝，则新promise就拒绝
         return new Promise1((resolve, reject) => {
+            // 如果不是数组，则返回一个拒绝的promise
+            if(!Array.isArray(promises))
+            {
+                reject(new TypeError("Argument is not iterable"));
+            }
+            // 如果数组长度为0，则返回一个解决的promise
+            if(promises.length === 0)
+            {
+                resolve(promises);
+            }
             let results = [];
-            promises.forEach((promise) => {
-                promise.then((value) => {
-                    results.push(value);
-                    if(results.length === promises.length){
+            let count = 0;
+            promises.forEach((promise,index) => {
+                // resolve会把非promise转成promise，promise直接用
+                let realPromise = Promise1.resolve(promise);
+                realPromise.then((value) => {
+                    // 不能用push，不然返回的结果和传入的顺序不一致
+                    // results.push(value);
+                    // if(results.length === promises.length){
+                    //     resolve(results);
+                    // }
+                    results[index] = value;
+                    // 当解决的次数和数组的长度一致时，就返回结果
+                    if (++count === promises.length) {
                         resolve(results);
                     }
                 }, (reason) => {
@@ -314,10 +392,49 @@ class Promise1{
         // 返回一个新的promise，实现竞速效果
         // 遍历所有promise，只要有一个promise解决或拒绝，新promise就解决或拒绝
         return new Promise1((resolve, reject) => {
+            // 如果不是数组，直接拒绝
+            if(!Array.isArray(promises))
+            {
+                reject(new TypeError("Argument is not iterable"));
+            }
             promises.forEach((promise) => {
-                promise.then(resolve, reject);
+                // resolve会把非promise转成promise，promise直接用
+                let realPromise = Promise1.resolve(promise);
+                realPromise.then(resolve, reject);
             });
         });
+    }
+
+    /**
+    * @description 静态方法any
+    * @param promises  promise数组
+    * @return void
+    * @status public
+    */
+    static any(promises){
+        return new Promise((resolve, reject) => {
+            if(!Array.isArray(promises))
+            {
+                reject(new TypeError("Argument is not iterable"));
+            }
+            if(promises.length === 0)
+            {
+                reject(new AggregateError(promises,"All promises were rejected"));
+            }
+            promises.forEach((promise,index) => {
+                // resolve会把非promise转成promise，promise直接用
+                let realPromise = Promise1.resolve(promise);
+                const errors = [];
+                let count = 0;
+                realPromise.then(resolve, err=>{
+                    errors[index] = err;
+                    if(++count === promises.length)
+                    {
+                        reject(new AggregateError(errors,"All promises were rejected"));
+                    }
+                });
+            });
+        })
     }
 
     /**
@@ -326,9 +443,20 @@ class Promise1{
     * @return void
     * @status public
     */
-    static catch(onRejected){
+    catch(onRejected){
         return this.then(null, onRejected);
     }
+
+    /**
+    * @description 
+    * @param 
+    * @return void
+    * @status public
+    */
+    finally(onFinally){
+        return this.then(onFinally,onFinally);
+    }
+    
 
     /**
     * @description 异步执行函数
@@ -461,6 +589,58 @@ class Promise1{
 //  * Chaining cycle detected for promise
 //  */
 
+// // 测试catch方法和finally方法
+// let promise1 = new Promise1((resolve, reject) => {
+//     throw "抛出异常"
+// });
+// promise1.catch(err=>{
+//     console.log(err);
+// }).finally(()=>{
+//     console.log("finally");
+// })
+/**
+ * 输出：
+ * 抛出异常
+ * finally
+ */
+
+// 测试静态方法resolve
+// Promise1.resolve(2).then((res)=>{
+//     console.log(res);
+// })
+// /**
+//  * 输出：
+//  * 2
+//  */
+
+// 测试静态方法reject
+// let promise1 = Promise1.reject(new Promise1((resolve,reject)=>{
+//     resolve(1)
+// }))
+// promise1.then((res)=>{
+//     console.log("成功");
+// },(err)=>{
+//     console.log("失败");
+//     console.log(err);
+// })
+// /**
+//  * 输出：
+//  * 失败
+//  * Promise1 {PromiseState: 'fulfilled', PromiseResult: 1, #callbacks: Array(0)}
+//  */
+
+// 测试allSettled
+let promise1 = new Promise1((resolve, reject) => {
+    resolve("成功1")
+})
+let promise2 = new Promise1((resolve, reject) => {
+    reject("失败3")
+})
+Promise1.allSettled(1).then(res => {
+    console.log(res);
+},(err)=>{
+    console.log(err);
+})
 
 // Promise1.reject(new Promise1((resolve, reject) => {
 //     resolve("成功");
@@ -486,3 +666,14 @@ class Promise1{
 // Promise1.allSettled(promiseArr).then((res) => {
 //     console.log(res);
 // });
+
+module.exports = {
+    deferred() {
+        let res = {};
+        res.promise = new Promise1((resolve, reject) => {
+            res.resolve = resolve;
+            res.reject = reject;
+        });
+        return res;
+    }
+}
